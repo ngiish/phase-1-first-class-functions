28b71878fe2c521bd650e2d4c7c528ce7cff09b7
Isaac <govegeku00@gmail.com>
2023-10-16 13:14:38 +0300

    Completed Lab

---
+++ index.js
@@ -0,0 +1,17 @@
+
+  function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      
+    };
+  }
+  
